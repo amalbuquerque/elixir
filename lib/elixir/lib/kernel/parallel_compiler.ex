aa8ec356ad7bfbbbf700c885959bc6172dd1806e
André Albuquerque <andre.malbuq@gmail.com>
2024-01-18 19:27:35 +0000

    Revert "Introduce Kernel.ParallelCompiler.pmap/2 (#12899)"
    This reverts commit 22dcba75d2c4a5d4cb4c3d3dfab53a9f08744d6f.

--- lib/elixir/lib/kernel/parallel_compiler.ex
+++ lib/elixir/lib/kernel/parallel_compiler.ex
@@ -14,9 +14,16 @@ defmodule Kernel.ParallelCompiler do
 
   @doc """
   Starts a task for parallel compilation.
+
+  If you have a file that needs to compile other modules in parallel,
+  the spawned processes need to be aware of the compiler environment.
+  This function allows a developer to create a task that is aware of
+  those environments.
+
+  See `Task.async/1` for more information. The task spawned must be
+  always awaited on by calling `Task.await/1`
   """
-  # TODO: Deprecate this on Elixir v1.20.
-  @doc deprecated: "Use `pmap/2` instead"
+  @doc since: "1.6.0"
   def async(fun) when is_function(fun, 0) do
     {ref, task} = inner_async(fun)
     send(task.pid, ref)
@@ -25,80 +32,27 @@ defmodule Kernel.ParallelCompiler do
 
   defp inner_async(fun) do
     case :erlang.get(:elixir_compiler_info) do
-      {compiler_pid, file_pid} ->
-        ref = make_ref()
+      {compiler, _} ->
         file = :erlang.get(:elixir_compiler_file)
         dest = :erlang.get(:elixir_compiler_dest)
 
         {:error_handler, error_handler} = :erlang.process_info(self(), :error_handler)
         {_parent, checker} = Module.ParallelChecker.get()
 
-        task =
-          Task.async(fn ->
-            Module.ParallelChecker.put(compiler_pid, checker)
-            :erlang.put(:elixir_compiler_info, {compiler_pid, file_pid})
-            :erlang.put(:elixir_compiler_file, file)
-            dest != :undefined and :erlang.put(:elixir_compiler_dest, dest)
-            :erlang.process_flag(:error_handler, error_handler)
-
-            receive do
-              ^ref -> fun.()
-            end
-          end)
-
-        send(compiler_pid, {:async, task.pid})
-        {ref, task}
+        Task.async(fn ->
+          send(compiler, {:async, self()})
+          Module.ParallelChecker.put(compiler, checker)
+          :erlang.put(:elixir_compiler_info, {compiler, self()})
+          :erlang.put(:elixir_compiler_file, file)
+          dest != :undefined and :erlang.put(:elixir_compiler_dest, dest)
+          :erlang.process_flag(:error_handler, error_handler)
+          fun.()
+        end)
 
       :undefined ->
         raise ArgumentError,
               "cannot spawn parallel compiler task because " <>
                 "the current file is not being compiled/required"
-    end
-  end
-
-  @doc """
-  Perform parallel compilation of `collection` with `fun`.
-
-  If you have a file that needs to compile other modules in parallel,
-  the spawned processes need to be aware of the compiler environment.
-  This function allows a developer to perform such tasks.
-  """
-  @doc since: "1.16.0"
-  def pmap(collection, fun) when is_function(fun, 1) do
-    ref = make_ref()
-
-    # We spawn a series of tasks for parallel processing.
-    # The tasks are waiting until we give the go ahead.
-    refs_tasks =
-      Enum.map(collection, fn item ->
-        inner_async(fn -> fun.(item) end)
-      end)
-
-    # Notify the compiler we are waiting on the tasks.
-    {compiler_pid, file_pid} = :erlang.get(:elixir_compiler_info)
-    defining = :elixir_module.compiler_modules()
-    on = Enum.map(refs_tasks, fn {_ref, %{pid: pid}} -> pid end)
-    send(compiler_pid, {:waiting, :pmap, self(), ref, file_pid, on, defining, :raise})
-
-    # Now we allow the tasks to run. This step is not strictly
-    # necessary but it makes compilation more deterministic by
-    # only allowing tasks to run once we are waiting.
-    tasks =
-      Enum.map(refs_tasks, fn {ref, task} ->
-        send(task.pid, ref)
-        task
-      end)
-
-    # Await tasks and notify the compiler they are done. We could
-    # have the tasks report directly to the compiler, which in turn
-    # would notify us, but that would require reimplementing await_many,
-    # and copying the results across boundaries, so we don't.
-    res = Task.await_many(tasks, :infinity)
-    send(compiler_pid, {:available, :pmap, on})
-
-    # Only run once the compiler lets us, to avoid unbounded parallelism.
-    receive do
-      {^ref, _result} -> res
     end
   end
 
@@ -585,7 +539,7 @@ defmodule Kernel.ParallelCompiler do
     nilify_empty_or_sort(
       for %{pid: file_pid} <- files,
           {pid, {_, _, ^file_pid, on, _, _}} <- waiting_list,
-          is_atom(on) and not defining?(on, waiting_list),
+          not defining?(on, waiting_list),
           do: {pid, :not_found}
     )
   end
@@ -593,7 +547,7 @@ defmodule Kernel.ParallelCompiler do
   defp deadlocked(waiting_list, type, defining?) do
     nilify_empty_or_sort(
       for {pid, {_, _, _, on, _, ^type}} <- waiting_list,
-          is_atom(on) and defining?(on, waiting_list) == defining?,
+          defining?(on, waiting_list) == defining?,
           do: {pid, :deadlock}
     )
   end
@@ -615,8 +569,8 @@ defmodule Kernel.ParallelCompiler do
         new_spawned = Map.put(spawned, ref, pid)
         wait_for_messages(queue, new_spawned, waiting, files, result, warnings, errors, state)
 
-      {:available, kind, on} ->
-        {available, result} = update_result(result, kind, on, :done)
+      {:available, kind, module} ->
+        {available, result} = update_result(result, kind, module, true)
 
         spawn_workers(
           available ++ queue,
@@ -634,6 +588,7 @@ defmodule Kernel.ParallelCompiler do
 
         # Release the module loader which is waiting for an ack
         send(child, {ref, :ack})
+
         {available, result} = update_result(result, :module, module, binary)
 
         spawn_workers(
@@ -652,7 +607,7 @@ defmodule Kernel.ParallelCompiler do
         send(child, {ref, :not_found})
         spawn_workers(queue, spawned, waiting, files, result, warnings, errors, state)
 
-      {:waiting, kind, child_pid, ref, file_pid, on, defining, deadlock} ->
+      {:waiting, kind, child_pid, ref, file_pid, on, defining, deadlock?} ->
         # If we already got what we were waiting for, do not put it on waiting.
         # If we're waiting on ourselves, send :found so that we can crash with
         # a better error.
@@ -663,7 +618,7 @@ defmodule Kernel.ParallelCompiler do
             send(child_pid, {ref, :found})
             {waiting, files, result}
           else
-            waiting = Map.put(waiting, child_pid, {kind, ref, file_pid, on, defining, deadlock})
+            waiting = Map.put(waiting, child_pid, {kind, ref, file_pid, on, defining, deadlock?})
             files = update_timing(files, file_pid, :compiling)
             result = Map.put(result, {kind, on}, [child_pid | available_or_pending])
             {waiting, files, result}
--- lib/elixir/test/elixir/kernel/parallel_compiler_test.exs
+++ lib/elixir/test/elixir/kernel/parallel_compiler_test.exs
@@ -363,7 +363,7 @@ defmodule Kernel.ParallelCompilerTest do
       purge([FooCircular, BarCircular])
     end
 
-    test "handles pmap compilation" do
+    test "handles async compilation" do
       [foo, bar] =
         write_tmp(
           "async_compile",
@@ -371,11 +371,10 @@ defmodule Kernel.ParallelCompilerTest do
           defmodule FooAsync do
             true = Code.can_await_module_compilation?()
 
-            [BarAsync] =
-              Kernel.ParallelCompiler.pmap([:ok], fn :ok ->
-                true = Code.can_await_module_compilation?()
-                BarAsync.__info__(:module)
-              end)
+            Kernel.ParallelCompiler.async(fn ->
+              true = Code.can_await_module_compilation?()
+              BarAsync.__info__(:module)
+            end)
           end
           """,
           bar: """
@@ -395,15 +394,17 @@ defmodule Kernel.ParallelCompilerTest do
       purge([FooAsync, BarAsync])
     end
 
-    test "handles pmap deadlocks" do
+    test "handles async deadlocks" do
       [foo, bar] =
         write_tmp(
           "async_deadlock",
           foo: """
           defmodule FooAsyncDeadlock do
-            Kernel.ParallelCompiler.pmap([:ok], fn :ok ->
+            Kernel.ParallelCompiler.async(fn ->
               BarAsyncDeadlock.__info__(:module)
             end)
+
+            BarAsyncDeadlock.__info__(:module)
           end
           """,
           bar: """
@@ -415,9 +416,9 @@ defmodule Kernel.ParallelCompilerTest do
 
       capture_io(:stderr, fn ->
         fixtures = [foo, bar]
-        assert {:error, [bar_error, foo_error], []} = Kernel.ParallelCompiler.compile(fixtures)
-        assert {^bar, nil, "deadlocked waiting on module FooAsyncDeadlock"} = bar_error
-        assert {^foo, nil, "deadlocked waiting on pmap [#PID<" <> _} = foo_error
+        assert {:error, [foo_error, bar_error], []} = Kernel.ParallelCompiler.compile(fixtures)
+        assert bar_error == {bar, nil, "deadlocked waiting on module FooAsyncDeadlock"}
+        assert foo_error == {foo, nil, "deadlocked waiting on module BarAsyncDeadlock"}
       end)
     end
 
